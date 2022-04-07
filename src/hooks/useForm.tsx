d5384b213876a2939809f2f7dcb31169a91be96d
stargazing4122 <jadenyeiden@gmail.com>
2022-04-07 00:09:55 -0500

    useForm added

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,22 @@
+import { ChangeEvent, useState } from "react"
+
+export const useForm = <T extends Object>( initialState: T ) => {
+  const [formValues, setFormValues] = useState(initialState);
+
+  const handleInputChange = ( e: ChangeEvent<HTMLInputElement>) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const resetForm = () => {
+    setFormValues( initialState );
+  }
+
+  return {
+    formValues,
+    handleInputChange,
+    resetForm,
+  }
+}
